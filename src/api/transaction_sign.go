f1d65b7683cc695cb6774e716a237e8ff82d3abd
therealssj <therealssj@protonmail.com>
2019-06-21 01:45:38 +0530

    remove txn sign input index check

--- src/api/transaction_sign.go
+++ src/api/transaction_sign.go
@@ -108,10 +108,6 @@ func (r *TransactionSignRequest) validate() error {
 		if input.Hash == "" {
 			return errors.New("input hash cannot be empty")
 		}
-
-		if input.Index == nil {
-			return errors.New("input index cannot be empty")
-		}
 	}
 
 	for _, output := range r.TransactionOutputs {
